cdc90a077efac494c9ae4f391c9d41c74ce5869f
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2022-12-03 00:36:10 +0200

    software-stack/arena: Add padding to AArch64 bss section
    Bug from qemu causes SEGFAULT for AArch64 bss sections that use .lcomm.
    This commit adds some padding that gets rid off of the issue. For
    executables with a greater text section this step is not required, as
    the padding needed by qemu in order to run fine is inversely
    proportional to the length of the .text section
    
    GitHub-Fixes: #57
    
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

--- content/chapters/software-stack/lab/solution/basic-syscall/arm/hello.s
+++ content/chapters/software-stack/lab/solution/basic-syscall/arm/hello.s
@@ -2,6 +2,13 @@
 
     .lcomm buffer, 128
 
+    /* 
+     * We need to add some padding due to a QEMU bug, present
+     * on the 4.2.1 version, that produces a SEGFAULT;
+     * the program works perfectly fine otherwise
+     */
+    .p2align 12
+
 .equ len, 128
 
 .section .rodata
