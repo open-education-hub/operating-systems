c80b928d03df2a8ef225615e65e1633f5453ff4c
Adrian Șendroiu <molecula2788@gmail.com>
2022-12-17 15:19:32 +0200

    app-interact/lab: linter fixes for get_battery_level.py skeleton

--- content/chapters/app-interact/lab/support/dbus/get_battery_level.py
+++ content/chapters/app-interact/lab/support/dbus/get_battery_level.py
@@ -1,13 +1,14 @@
 import dbus
 
 # TODO: get the system bus
-system_bus = TODO
+system_bus = dbus.TODO
 
-# TODO: use "get_object" on the "org.freedesktop.UPower" connection to obtain a handle to the "/org/freedesktop/UPower/devices/DisplayDevice" object
-proxy = system_bus.get_object(TODO)
+# TODO: use "get_object" on the "org.freedesktop.UPower" connection
+# to obtain a handle to the "/org/freedesktop/UPower/devices/DisplayDevice" object
+proxy = system_bus.get_object()
 
 # TODO: call the "Get" method on the interface "org.freedesktop.DBus.Properties"
 # and the arguments "org.freedesktop.UPower.Device" and "Percentage"
-level = proxy.Get(TODO)
+level = proxy.Get()
 
 print(level)
