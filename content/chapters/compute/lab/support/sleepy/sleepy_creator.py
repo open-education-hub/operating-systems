a4ea15c6bad797af13839684a02afeb3e9d5ec4b
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2022-12-28 14:16:42 +0200

    compute/lab: Replace `sleep()` with `wait()` call
    Support and solution files for `sleepy/sleepy_creator.py` demonstrate
    waiting for child processes by inducing the main process in a
    subsequent sleep.
    
    A better demonstration of the concept of orphan processes and waiting
    for children is by explicitly making a call for the `wait()` function.
    
    This commit replaces the `sleep()` call with `wait()`, therefore
    providing a clearer demo for subprocess waiting, that would be further
    continued in the lab, in the `wait-for-me` and `fork` practice items.
    
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

--- content/chapters/compute/lab/support/sleepy/sleepy_creator.py
+++ content/chapters/compute/lab/support/sleepy/sleepy_creator.py
@@ -5,11 +5,10 @@ NUM_SLEEPS = 10
 
 
 def main():
-    # TODO 1: create 10 `sleep 1000` processes using `subprocess.Popen`
+    # TODO 1: Create 10 `sleep 1000` processes using `subprocess.Popen`
     # Use the documentation: https://docs.python.org/3/library/subprocess.html#subprocess.Popen
 
-    # TODO 2: Make this script also wait for 1000 seconds. Use `sleep` from the
-    # `time` module.
+    # TODO 2: Make the current process wait for its child processes.
 
     pass
 
