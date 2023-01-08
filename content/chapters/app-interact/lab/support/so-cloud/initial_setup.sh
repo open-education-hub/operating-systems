685f98c789c8dd9ba3bd1e178063b4dd440232c2
Adrian Șendroiu <molecula2788@gmail.com>
2023-01-08 17:45:44 +0200

    app-interact/lab: change url for ubuntu qcow2 image

--- content/chapters/app-interact/lab/support/so-cloud/initial_setup.sh
+++ content/chapters/app-interact/lab/support/so-cloud/initial_setup.sh
@@ -7,13 +7,14 @@ set -e
 sudo modprobe nbd max_part=16
 
 # Downloading disk templates.
-DISKS=('https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img' 'ubuntu_22.04')
+# Original from https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img
+DISKS=('https://elf.cs.pub.ro/so/res/mv/ubuntu-22.04-server-cloudimg-amd64.qcow2' 'ubuntu_22.04')
 
 len=${#DISKS[@]}
 for ((i = 0; i < len/2; i++)); do
     url=${DISKS[2*i]}
     name=${DISKS[2*i+1]}
-    wget "$url" -O "./disk-templates/$name/$name.qcow2"
+    wget --no-check-certificate "$url" -O "./disk-templates/$name/$name.qcow2"
 done
 
 # Pull docker images
