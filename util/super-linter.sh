d9f9bab6d6168a15736888866c62f5364090c567
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2023-02-04 20:37:56 +0200

    util: Add super-linter script to be able to run it locally
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>
    Co-authored-by: Teodor Dutu <teodor.dutu@gmail.com>

---
+++ util/super-linter.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# PATH_TO_CODEBASE: The path to the codebase you want to lint. Don't use relative paths.
+# PATH_TO_LINT_RULES: The path to the lint rules you want to use. Don't use relative paths.
+# Example: ./super-linter.sh /path/to/codebase/to/be/check /path/to/.github/workflows from the root of the repo
+
+PATH_TO_CODEBASE=$(realpath "$1")
+PATH_TO_LINT_RULES=$(realpath "$2")
+
+# Exit if codebase is not provided.
+if [ -z "$PATH_TO_CODEBASE" ]; then
+  echo "Please provide the path to the codebase you want to lint."
+  exit 1
+fi
+
+# Exit if lint rules are not provided.
+if [ -z "$PATH_TO_LINT_RULES" ]; then
+  echo "Please provide the path to the lint rules you want to use."
+  exit 1
+fi
+
+# Exit if docker is not installed.
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: docker is not installed.' >&2
+  exit 1
+fi
+
+docker run --rm \
+    -e RUN_LOCAL=true \
+    -e VALIDATE_GITHUB_ACTIONS=false \
+    -e VALIDATE_CLANG_FORMAT=false \
+    -e USE_FIND_ALGORITHM=true \
+    -e VALIDATE_ALL_CODEBASE=false \
+    -e MARKDOWN_CONFIG_FILE=../../../tmp/github/config/config.json \
+    -e MARKDOWN_CUSTOM_RULE_GLOBS=../../../../tmp/github/rules/rules.js \
+    -e LOG_LEVEL=WARN \
+    -v "$PATH_TO_LINT_RULES":/tmp/github \
+    -v "$PATH_TO_CODEBASE":/tmp/lint \
+    github/super-linter:v4
