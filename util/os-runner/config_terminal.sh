f60eeb42babf4ea75d4f5fd3ad270f4de39f91a5
Alexandru Apostolescu <73755280+Alex-deVis@users.noreply.github.com>
2023-03-03 09:27:57 +0200

    Update util/os-runner/config_terminal.sh
    Co-authored-by: Adrian Șendroiu <molecula2788@gmail.com>

--- util/os-runner/config_terminal.sh
+++ util/os-runner/config_terminal.sh
@@ -2,7 +2,7 @@
 
 # Configure terminal
 # shellcheck disable=2154
-PS1='🐳 ${debian_chroot:+($debian_chroot)}\[\033[01;36m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$'
+PS1='🐳 ${debian_chroot:+($debian_chroot)}\[\033[01;36m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 PROMPT_DIRTRIM=3
 alias python=python3
 
