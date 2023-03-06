bcdad7ccbc2dd2411667d426c086d217612b4b9e
Teodor Dutu <teodor.dutu@gmail.com>
2023-03-06 22:12:10 +0800

    util: Install `dmd` using the official installation script
    This also activates the compiler silently, without modifying the prompt.
    
    Signed-off-by: Teodor Dutu <teodor.dutu@gmail.com>

--- util/os-runner/install-dmd.sh
+++ util/os-runner/install-dmd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wget https://netcologne.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
-apt-get update --allow-insecure-repositories
-apt-get -y --allow-unauthenticated install --reinstall d-apt-keyring
-apt-get update && sudo apt-get install -y dmd-compiler dub
+curl -fsS https://dlang.org/install.sh | bash -s dmd
+# shellcheck source=/dev/null
+source "$(find ~/dlang -maxdepth 1 -name 'dmd-*')/activate"
