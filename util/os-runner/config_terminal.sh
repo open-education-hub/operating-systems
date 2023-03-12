a0ed9e6022e7dc31daf68367ba3bf10fb113b642
Iulian277 <iulian27_marius@yahoo.com>
2023-03-12 20:11:13 +0200

    added update-alternatives for python symbolic link

--- util/os-runner/config_terminal.sh
+++ util/os-runner/config_terminal.sh
@@ -4,7 +4,6 @@
 # shellcheck disable=2154
 PS1='🐳 ${debian_chroot:+($debian_chroot)}\[\033[01;36m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 PROMPT_DIRTRIM=3
-alias python=python3
 
 # Add aliases for labs
 # shellcheck disable=1007
