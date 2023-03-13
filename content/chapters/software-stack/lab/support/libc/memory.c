c1bf6888208f372bb3933070670e6bafb8e7b0ff
Razvan Deaconescu <razvan.deaconescu@upb.ro>
2023-03-13 04:28:30 +0200

    software-stack/lab: Add memory.c skeleton support file
    Signed-off-by: Razvan Deaconescu <razvan.deaconescu@upb.ro>

---
+++ content/chapters/software-stack/lab/support/libc/memory.c
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#include <stdlib.h>
+
+int main(void)
+{
+	void *p;
+
+	/*
+	 * TODO: Use malloc() to allocate memory of different sizes.
+	 * Store result in p.
+	 * Use free() to deallocate memory.
+	 * Use ltrace and strace command line tools to monitor library
+	 * and system calls.
+	 */
+
+	return 0;
+}
