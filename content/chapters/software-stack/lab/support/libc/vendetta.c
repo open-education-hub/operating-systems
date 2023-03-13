d0fbb1b9cea35e40d4be2b92dab68330be75a1bd
Razvan Deaconescu <razvan.deaconescu@upb.ro>
2023-03-13 04:28:42 +0200

    software-stack/lab: Add vendetta.c skeleton support file
    Signed-off-by: Razvan Deaconescu <razvan.deaconescu@upb.ro>

---
+++ content/chapters/software-stack/lab/support/libc/vendetta.c
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <math.h>
+
+int main(void)
+{
+	/*
+	 * TODO: Use standard C library functions.
+	 * Be as creative as you can.
+	 */
+
+	return 0;
+}
