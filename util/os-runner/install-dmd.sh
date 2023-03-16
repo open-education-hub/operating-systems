1bc6318b26708fd2ecdf7a6800a1e4aa89909ce0
Adrian Șendroiu <molecula2788@gmail.com>
2023-03-17 00:37:16 +0200

    infra/os_runner: increase connect-timeout to 15 seconds

--- util/os-runner/install-dmd.sh
+++ util/os-runner/install-dmd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -fsS https://dlang.org/install.sh | bash -s dmd
+curl -fsS https://dlang.org/install.sh | sed 's/--connect-timeout 5/--connect-timeout 15/g' | bash -s dmd
 # shellcheck source=/dev/null
 source "$(find ~/dlang -maxdepth 1 -name 'dmd-*')/activate"
