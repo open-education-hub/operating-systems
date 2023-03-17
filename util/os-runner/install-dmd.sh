8acb73e76ba42a53d2ed586ae30e0cf24f901bf4
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2023-03-17 15:38:32 +0200

    util/os-runner: Add pipeline to publish os-runner image
    - Add pipeline to build os-runner image for linux/amd64 and linux/arm64
    - Update install-dmd.sh to install ldc for arm64
    - Update Makefile to pull image from ghcr
    
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- util/os-runner/install-dmd.sh
+++ util/os-runner/install-dmd.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-curl -fsS https://dlang.org/install.sh | sed 's/--connect-timeout 5/--connect-timeout 15/g' | bash -s dmd
-# shellcheck source=/dev/null
-source "$(find ~/dlang -maxdepth 1 -name 'dmd-*')/activate"
+if [ "$(uname -m)" = "x86_64" ]; then
+  curl -fsS https://dlang.org/install.sh | sed 's/--connect-timeout 5/--connect-timeout 15/g' | bash -s dmd
+  # shellcheck source=/dev/null
+  source "$(find ~/dlang -maxdepth 1 -name 'dmd-*')/activate"
+fi
+
+
+if [ "$(uname -m)" = "aarch64" ]; then
+  curl -fsS https://dlang.org/install.sh | sed 's/--connect-timeout 5/--connect-timeout 15/g' | bash -s ldc
+  # shellcheck source=/dev/null
+  source "$(find ~/dlang -maxdepth 1 -name 'ldc-*')/activate"
+fi
