ebb8aad0ae08e44906d77b92716100da66c85c13
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-03-24 16:14:51 +0200

    Updated docker configure files

--- util/os-runner/config_terminal.sh
+++ util/os-runner/config_terminal.sh
@@ -2,8 +2,8 @@
 
 # Configure terminal
 # shellcheck disable=2154
-PS1='🐳 ${debian_chroot:+($debian_chroot)}\[\033[01;36m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 PROMPT_DIRTRIM=3
+PS1='🐳 \[\033[01;36m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
 # Add aliases for labs
 # shellcheck disable=1007
