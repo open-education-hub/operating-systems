fc305e87da1c19df0c8c3d8a366c4c768de12ec9
Teodor Juravlea <teodorjuravlea@gmail.com>
2023-03-28 19:08:16 +0300

    assignments/mini-libc: Use `errno` in implementation of `write()` and `read()`
    Changed the implementation of `write()` and `read()` to set `errno`,
    as per their manual pages.
    
    Signed-off-by: Teodor Juravlea <teodorjuravlea@gmail.com>

--- content/assignments/mini-libc/src/io/read_write.c
+++ content/assignments/mini-libc/src/io/read_write.c
@@ -3,13 +3,28 @@
 #include <internal/io.h>
 #include <internal/syscall.h>
 #include <internal/types.h>
+#include <errno.h>
 
 ssize_t write(int fd, const void *buf, size_t len)
 {
-	return syscall(__NR_write, fd, buf, len);
+	int ret = syscall(__NR_write, fd, buf, len);
+
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
+
+	return ret;
 }
 
 ssize_t read(int fd, void *buf, size_t len)
 {
-	return syscall(__NR_read, fd, buf, len);
+	int ret =  syscall(__NR_read, fd, buf, len);
+
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
+
+	return ret;
 }
